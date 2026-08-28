c89d245d1e341a887c72c316fcc01facc9221d65
Arnur Omirserikov <arnuromirserikov@Arnurs-MacBook-Air.local>
2026-08-28 12:43:01 +0500

    Added Courses Section with Course Cards in Home

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,9 +1,42 @@
-import { createContext } from "react";
+import React from "react";
+import { dummyCourses } from "../assets/assets";
+import { useNavigate } from "react-router-dom";
 
-const AppContext = createContext();
+export const AppContext = React.createContext();
 
 export const AppContextProvider = (props) => {
-  const value = {};
+  const currency = import.meta.env.VITE_CURRENCY;
+  const navigate = useNavigate();
+
+  const [allCourses, setAllCourses] = React.useState([]);
+
+  // Fetch All Courses
+  const fetchAllCourses = () => {
+    setAllCourses(dummyCourses);
+  };
+
+  // Function to calculate average rating of course
+  const calculateRating = (course) => {
+    if (course.courseRatings.length === 0) {
+      return 0;
+    }
+    let totalRating = 0;
+    course.courseRatings.forEach((rating) => {
+      totalRating += rating.rating;
+    });
+    return totalRating / course.courseRatings.length;
+  };
+
+  React.useEffect(() => {
+    fetchAllCourses();
+  }, []);
+
+  const value = {
+    currency,
+    allCourses,
+    navigate,
+    calculateRating,
+  };
   return (
     <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
   );
