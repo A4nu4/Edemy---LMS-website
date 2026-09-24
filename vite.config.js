7995807fc648e935e13387eb5909448f1ad4d2e9
Arnur Omirserikov <arnuromirserikov@Arnurs-MacBook-Air.local>
2026-09-24 17:41:59 +0500

    brotli compress

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import viteCompression from "vite-plugin-compression";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    viteCompression({ algorithm: "brotliCompress", threshold: 1024 }),
+  ],
 });
